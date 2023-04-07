ed046476d8341a7fcbd56d15cf1faf2303f1dc2f
EtihadStadium <mancity@mailbox.org>
2023-04-07 14:44:40 +0900

    Add random duration

--- app.js
+++ app.js
@@ -1,13 +1,16 @@
 const body = document.querySelector("body");
+const MIN_DURATION = 10;
 function makeSnowflake() {
     if (!body) return;
     const snowflake = document.createElement("div");
     const delay = Math.random() * 10;
     const initialOpacity = Math.random();
+    const duration = Math.random() * 20 + MIN_DURATION;
     snowflake.classList.add("snowflake");
     snowflake.style.left = `${Math.random() * window.innerWidth}px`;
     snowflake.style.animationDelay = `${delay}s`;
     snowflake.style.opacity = initialOpacity;
+    snowflake.style.animation = `fall ${duration}s linear`;
     body.appendChild(snowflake);
 }
 makeSnowflake();
