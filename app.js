ae506764679899fd8f08627730265c762fdeb7df
EtihadStadium <mancity@mailbox.org>
2023-04-07 14:40:47 +0900

    Add random opacity

--- app.js
+++ app.js
@@ -3,9 +3,11 @@ function makeSnowflake() {
     if (!body) return;
     const snowflake = document.createElement("div");
     const delay = Math.random() * 10;
+    const initialOpacity = Math.random();
     snowflake.classList.add("snowflake");
     snowflake.style.left = `${Math.random() * window.innerWidth}px`;
     snowflake.style.animationDelay = `${delay}s`;
+    snowflake.style.opacity = initialOpacity;
     body.appendChild(snowflake);
 }
 makeSnowflake();
